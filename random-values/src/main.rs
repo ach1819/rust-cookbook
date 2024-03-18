17e1be46ad7a7d375f2feb83da549897464bfddb
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-17 21:54:39 -0500

    feat: Generating range numbers with uniform

--- random-values/src/main.rs
+++ random-values/src/main.rs
@@ -1,15 +1,14 @@
+use rand::distributions::{Distribution, Uniform};
 use rand::Rng;
 
 fn main() {
-
     genarate_random_types();
     generate_random_range_numbers();
-
+    // This could take some time
+    generate_random_with_uniform();
 }
 
-
 fn genarate_random_types() {
-
     println!("*** Starts - Generation random by type ***");
     let mut rng = rand::thread_rng();
 
@@ -21,17 +20,25 @@ fn genarate_random_types() {
     println!("Random u32: {}", rng.gen::<u32>());
     println!("Random i32: {}", rng.gen::<i32>());
     println!("Random float: {}", rng.gen::<f64>());
-
 }
 
 fn generate_random_range_numbers() {
-
     println!("\n*** Starts - Generation random by range ***");
     let mut rng = rand::thread_rng();
 
     println!("Integer: {}", rng.gen_range(0..10));
     println!("Float: {}", rng.gen_range(0.0..10.0));
-
 }
 
+fn generate_random_with_uniform() {
+    let mut rng = rand::thread_rng();
+    let die = Uniform::from(1..7);
 
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
+}
