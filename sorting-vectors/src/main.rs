edbb0b61df5db7302cf315c60d9c10849a698a13
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-19 20:49:22 -0500

    feat: Sorting struct, by name and/or by age

--- sorting-vectors/src/main.rs
+++ sorting-vectors/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     sort_integers_vector();
     sort_float_vertor();
+    sort_struct_vector();
 }
 
 fn sort_integers_vector() {
@@ -20,4 +21,48 @@ fn sort_float_vertor() {
     assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
 
     println!("Sorted vector(float): {:?}", vec);
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Person { name, age }
+    }
+}
+
+fn sort_struct_vector() {
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1),
+    ];
+
+    people.sort();
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25),
+        ]
+    );
+    println!("\nPeople sorted by name:\n{:?}", people);
+
+    people.sort_by(|a, b| b.age.cmp(&a.age));
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("Zoe".to_string(), 25),
+            Person::new("John".to_string(), 1),
+        ]
+    );
+    println!("\nPeople sorted by age:\n{:?}", people);
 }
