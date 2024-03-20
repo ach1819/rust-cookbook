1d7f3a85e0085368e9b124d214b156080b9ce343
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-19 20:37:17 -0500

    feat: Sorting float vector

--- sorting-vectors/src/main.rs
+++ sorting-vectors/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     sort_integers_vector();
+    sort_float_vertor();
 }
-
 
 fn sort_integers_vector() {
     let mut vec = vec![1, 5, 10, 2, 15];
@@ -10,4 +10,14 @@ fn sort_integers_vector() {
 
     assert_eq!(vec, vec![1, 2, 5, 10, 15]);
     println!("Sorted vector: {:?}", vec);
+}
+
+fn sort_float_vertor() {
+    let mut vec = vec![1.1, 1.15, 5.5, 1.123, 2.0];
+
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
+
+    println!("Sorted vector(float): {:?}", vec);
 }
