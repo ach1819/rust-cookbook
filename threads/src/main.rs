10f05097bcc8e83beec9a5f633874c3cdcfac444
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-23 11:30:25 -0500

    feat: parallel pipeline

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,5 +1,14 @@
+extern crate crossbeam;
+extern crate crossbeam_channel;
+
+use std::thread;
+use std::time::Duration;
+
+use crossbeam_channel::bounded;
+
 fn main() {
     spawn_short_lived_thread();
+    parallel_pipeline();
 }
 
 fn spawn_short_lived_thread() {
@@ -29,4 +38,47 @@ fn find_max(arr: &[i32]) -> Option<i32> {
         Some(max_l.max(max_r))
     })
     .unwrap()
+}
+
+fn parallel_pipeline() {
+    let (snd1, rcv1) = bounded(1);
+    let (snd2, rcv2) = bounded(2);
+    let n_msgs = 4;
+    let n_workers = 2;
+
+    crossbeam::scope(|s| {
+        // Producer thread
+        s.spawn(|_| {
+            for i in 0..n_msgs {
+                snd1.send(i).unwrap();
+                println!("Source sent {}", i);
+            }
+
+            // Close the channel - this is necesarry to exit!
+            // the for-loop in the wroker
+            drop(snd1);
+        });
+
+        // Parallel processing by 2 threads
+        for _ in 0..n_workers {
+            // Send to sink, receive from source
+            let (sendr, recvr) = (snd2.clone(), rcv1.clone());
+            // Spawn workers in separate threads
+            s.spawn(move |_| {
+                thread::sleep(Duration::from_millis(500));
+
+                for msg in recvr.iter() {
+                    println!("Worker {:?} received {}.", thread::current().id(), msg);
+                    sendr.send(msg * 2).unwrap();
+                }
+            });
+        }
+        // Clone the channel, otherwise sink will never
+        // exit the for-loop
+        drop(snd2);
+
+        for msg in rcv2.iter() {
+            println!("Sink received {}", msg);
+        }
+    }).unwrap();
 }
