acc99e35dd60a27000672371cc65e6f89988ee1f
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-26 20:02:43 -0500

    feat: Map-reduce in parallel

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -34,6 +34,7 @@ fn main() {
     test_any_element_match_given_predicate();
     search_item_given_predicate();
     sort_vector_parallel();
+    map_reduce_parallel();
 }
 
 fn spawn_short_lived_thread() {
@@ -344,4 +345,39 @@ fn sort_vector_parallel() {
     });
     vec.par_sort_unstable();
     println!("sort_vector_parallel - OK");
+}
+
+struct Person {
+    age: u32,
+}
+
+fn map_reduce_parallel() {
+    println!("\nmap_reduce_parallel - starts");
+    let v: Vec<Person> = vec![
+        Person { age: 23 },
+        Person { age: 19 },
+        Person { age: 42 },
+        Person { age: 17 },
+        Person { age: 17 },
+        Person { age: 31 },
+        Person { age: 30 },
+    ];
+
+    let num_over_30 = v.par_iter().filter(|&x| x.age > 30).count() as f32;
+    let sum_over_30 = v.par_iter()
+        .map(|x| x.age)
+        .filter(|&x| x > 30)
+        .reduce(|| 0, |x, y| x + y);
+
+    let alt_sum_30: u32 = v.par_iter()
+        .map(|x| x.age)
+        .filter(|&x| x > 30)
+        .sum();
+
+    let avg_over_30 = sum_over_30 as f32 / num_over_30;
+    let alt_avg_over_30 = alt_sum_30 as f32 / num_over_30;
+
+    assert!((avg_over_30 - alt_avg_over_30).abs() < std::f32::EPSILON);
+    println!("The average age of people older than 30 is {}", avg_over_30);
+
 }
