6fdb77c6a8a2898f737472ad85a8e38f0f877862
ach1819 <adrian.chavarria1819@gmail.com>
2024-03-28 14:25:12 -0500

    feat: Sign and verify a message with HMAC digest

--- cryptography/src/main.rs
+++ cryptography/src/main.rs
@@ -3,9 +3,13 @@ use std::io::{BufReader, Error, Read, Write};
 
 use data_encoding::HEXUPPER;
 use ring::digest::{Context, Digest, SHA256};
+use ring::error::Unspecified;
+use ring::hmac;
+use ring::rand::{self, SecureRandom};
 
 fn main() {
     calculate_sha_256_digist().expect("Error calculating sha 256");
+    sign_and_verify_message_hmac().expect("Error signing and verifying the message");
 }
 fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest, Error> {
     let mut context = Context::new(&SHA256);
@@ -39,5 +43,19 @@ fn calculate_sha_256_digist() -> Result<(), Error> {
 
     println!("SHA-256 digest is {}", HEXUPPER.encode(digest.as_ref()));
 
+    Ok(())
+}
+
+fn sign_and_verify_message_hmac()  -> Result<(), Unspecified>{
+    println!("\nsign_and_verify_message_hmac - starts");
+    let mut key_value = [0u8; 48];
+    let rng = rand::SystemRandom::new();
+    rng.fill(&mut key_value)?;
+    let key = hmac::Key::new(hmac::HMAC_SHA256, &key_value);
+
+    let message = "Legitimate and important message.";
+    let signature = hmac::sign(&key, message.as_bytes());
+    hmac::verify(&key, message.as_bytes(), signature.as_ref())?;
+    println!("sign_and_verify_message_hmac - OK");
     Ok(())
 }
