9282c7714fb235172baa3f32da76266436fc033d
ach1819 <adrian.chavarria1819@gmail.com>
2024-04-01 18:42:42 -0500

    feat: Examine the date and time

--- date-and-time/src/main.rs
+++ date-and-time/src/main.rs
@@ -1,11 +1,15 @@
-use std::{thread, time::{Duration, Instant}};
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
 
-use chrono::{DateTime, FixedOffset, Local, Utc};
+use chrono::{DateTime, Datelike, FixedOffset, Local, Timelike, Utc};
 
 fn main() {
     measure_elapsed_time_between();
     perform_checked_date_time_calculations();
     convert_local_time_to_timezone();
+    examine_date_time();
 }
 
 fn expensive_function() {
@@ -13,10 +17,12 @@ fn expensive_function() {
 }
 
 fn measure_elapsed_time_between() {
+    println!("\nmeasure_elapsed_time_between - starts");
     let start = Instant::now();
     expensive_function();
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is {:?}", duration);
+    println!("measure_elapsed_time_between - OK");
 }
 
 fn day_earlier(date_time: DateTime<Utc>) -> Option<DateTime<Utc>> {
@@ -28,13 +34,14 @@ fn perform_checked_date_time_calculations() {
     let now = Utc::now();
     println!("{}", now);
 
-    let almost_three_weeks_from_now = now.checked_add_signed(chrono::Duration::weeks(2))
+    let almost_three_weeks_from_now = now
+        .checked_add_signed(chrono::Duration::weeks(2))
         .and_then(|in_2weeks| in_2weeks.checked_add_signed(chrono::Duration::weeks(1)))
         .and_then(day_earlier);
 
     match almost_three_weeks_from_now {
         Some(x) => println!("{}", x),
-        None => eprintln!("Almost three weeks from now overflows!")
+        None => eprintln!("Almost three weeks from now overflows!"),
     }
 
     match now.checked_add_signed(chrono::Duration::max_value()) {
@@ -45,6 +52,7 @@ fn perform_checked_date_time_calculations() {
 }
 
 fn convert_local_time_to_timezone() {
+    println!("\nconvert_local_time_to_timezone - starts");
     let local_time = Local::now();
     let utc_time = chrono::Utc::now();
     let china_timezone = FixedOffset::east_opt(8 * 3600);
@@ -55,6 +63,44 @@ fn convert_local_time_to_timezone() {
         "Time in Hong Kong now is {}",
         utc_time.with_timezone(&china_timezone.unwrap())
     );
-    println!("Time in Rio de Janeiro now is {}", utc_time.with_timezone(&rio_timezone.unwrap()));
+    println!(
+        "Time in Rio de Janeiro now is {}",
+        utc_time.with_timezone(&rio_timezone.unwrap())
+    );
+    println!("convert_local_time_to_timezone - OK");
+}
 
+fn examine_date_time() {
+    println!("\nexamine_date_time - starts");
+    let now = chrono::Utc::now();
+
+    let (is_pm, hour) = now.hour12();
+
+    println!(
+        "The current UTC time is {:02}:{:02}:{:02} {}",
+        hour,
+        now.minute(),
+        now.second(),
+        if is_pm { "PM"} else { "AM" }
+    );
+
+    println!(
+        "And there have been {} seconds since midnight",
+        now.num_seconds_from_midnight()
+    );
+
+    let (is_common_era, year) = now.year_ce();
+    println!(
+        "The current UTC date is {}-{:02}-{:02} {:?} ({})",
+        year,
+        now.month(),
+        now.day(),
+        now.weekday(),
+        if is_common_era { "CE" } else { "BCE" }
+    );
+    println!(
+        "And the Common Era began {} days ago",
+        now.num_days_from_ce()
+    );
+    println!("examine_date_time - OK");
 }
